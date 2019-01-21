82dea032e94875ec575b3a29b61ff08b96735571
Matthew Kling <mattkling@gmail.com>
2019-01-21 12:44:10 -0800

    parallelize

--- code/species_edginess.R
+++ code/species_edginess.R
@@ -9,27 +9,20 @@ library(grid)
 library(gridExtra)
 
 
-source("E:/edges/edginess/ah2sp.R")
+setwd("e:/edges/edgy")
+
+source("code/ah2sp.R")
 
 
 
 # load species range maps
-spp <- c("Quercus douglasii", "Pseudotsuga menziesii", 
-         "Pinus albicaulis", "Pinus coulteri")
-f <- list.files("F:/little_trees/raw_data", 
+spf <- list.files("F:/little_trees/raw_data", 
                 recursive=T, full.names=T, pattern="shp")
-f <- f[grepl(paste(spp, collapse="|"), f)]
-
-d <- lapply(f, readOGR)
-names(d) <- basename(dirname(f))
-
-ext <- lapply(d, extent) %>%
-      Reduce("merge", .)
-
 
 
 # load climate data
 f <- list.files("F:/chelsa/derived", full.names=T)
+ext <- extent(-173, -50, 13, 72)
 wb <- stack(f[c(1,2,4)]) %>% crop(ext)
 f <- list.files("F:/chelsa/monthly48", full.names=T)
 jja <- stack(f[c(34,35,36)]) %>% crop(ext) %>% mean()
@@ -38,66 +31,74 @@ djf <- stack(f[c(37,40,41)]) %>% crop(ext) %>% mean()
 s <- stack(wb, jja, djf)
 names(s) <- c("AET", "CWD", "PPT", "JJA", "DJF")
 
-v <- values(s) 
-pca <- prcomp(na.omit(v), center=T, scale=T)
-pc <- s[[1:2]]
-pc[!is.na(pc)] <- pca$x[,1:2]
-names(pc) <- c("pc1", "pc2")
 
-l <- as.data.frame(pca$rotation)
-for(i in 1:length(l)) l[[i]] <- pca$sdev[i] * l[[i]]
-l$input <- rownames(l)
+# state borders, for plotting
+usa <- getData("GADM", country="USA", level=1) %>%
+      spTransform(crs(s)) %>%
+      fortify() %>%
+      mutate(group=paste("usa", group))
+can <- getData("GADM", country="CAN", level=1) %>%
+      spTransform(crs(s)) %>%
+      fortify() %>%
+      mutate(group=paste("can", group))
+mex <- getData("GADM", country="MEX", level=1) %>%
+      spTransform(crs(s)) %>%
+      fortify() %>%
+      mutate(group=paste("mex", group))
+borders <- rbind(usa, can, mex)
 
 
-# realized niche data
-niche <- function(x){
+
+niche <- function(spp){
+      # spp <- "Quercus douglasii"
       
-      cc <- stack(s, pc) %>% crop(x)
-      xr <- rasterize(x, cc[[1]])
-      xd <- stack(cc, xr) %>%
+      rng <- readOGR(spf[grepl(spp, spf)])
+      xd <- s %>% 
+            crop(rng) %>% 
+            mask(rng) %>%
             rasterToPoints() %>%
             as.data.frame() %>%
-            na.omit() %>%
-            rename(id=layer)
+            na.omit()
       
       # species-specific PCA
       spca <- xd %>% dplyr::select(AET:DJF) %>% scale(center=T, scale=T) %>% prcomp()
       spc <- spca$x
-      colnames(spc) <- paste0("s", colnames(spc))
+      #colnames(spc) <- paste0("s", colnames(spc))
       xd <- cbind(xd, spc)
       
       ld <- as.data.frame(spca$rotation)
-      for(i in 1:length(ld)) l[[i]] <- spca$sdev[i] * ld[[i]]
+      #for(i in 1:length(ld)) l[[i]] <- spca$sdev[i] * ld[[i]]
       ld$input <- rownames(ld)
       
-      x <- list(x=xd,
+      x <- list(spp=spp,
+                x=xd,
                 loadings=ld)
-      
       return(x)
 }
-
+ 
 
 
 # alpha hulls
-#box <- r[[2]]
 hulls <- function(box){
+      #box <- r[[2]]
+      
       require(FNN)
       
       x <- box$x
       
-      dupe <- duplicated(x[,c("pc1", "pc2", "sPC1", "sPC2")])
+      dupe <- duplicated(x[,c("PC1", "PC2")])
       xu <- x[!dupe,]
       
       dupe <- duplicated(x[,c("x", "y")])
       xg <- x[!dupe,]
       
       if(nrow(xu)>10000){
-            rng1 <- diff(range(xu$sPC1))
-            rng2 <- diff(range(xu$sPC2))
+            rng1 <- diff(range(xu$PC1))
+            rng2 <- diff(range(xu$PC2))
             xu <- xu %>%
-                  mutate(pc1b=plyr::round_any(sPC1, rng1/200),
-                         pc2b=plyr::round_any(sPC2, rng2/200)) %>%
-                  group_by(pc1b, pc2b) %>%
+                  mutate(PC1b=plyr::round_any(PC1, rng1/200),
+                         PC2b=plyr::round_any(PC2, rng2/200)) %>%
+                  group_by(PC1b, PC2b) %>%
                   sample_n(1) %>%
                   ungroup()
       }
@@ -105,45 +106,35 @@ hulls <- function(box){
             rng1 <- diff(range(xg$x))
             rng2 <- diff(range(xg$y))
             xg <- xg %>%
-                  mutate(pc1b=plyr::round_any(ecdf(x)(x), .005),
-                         pc2b=plyr::round_any(ecdf(y)(y), .005)) %>%
-                  group_by(pc1b, pc2b) %>%
+                  mutate(PC1b=plyr::round_any(ecdf(x)(x), .005),
+                         PC2b=plyr::round_any(ecdf(y)(y), .005)) %>%
+                  group_by(PC1b, PC2b) %>%
                   sample_n(1) %>%
                   ungroup()
       }
       
       # alpha hull
-      ah <- ahull(xu[,c("pc1", "pc2")], alpha=.5)
-      ah2 <- ahull(xu[,c("sPC1", "sPC2")], alpha=.5)
+      ah <- ahull(xu[,c("PC1", "PC2")], alpha=.5)
       ahg <- ahull(xg[,c("x", "y")], alpha=2)
       sp <- ah2sp(ah)
-      sp2 <- ah2sp(ah2)
       spg <- ah2sp(ahg, rnd=3)
       
       # sample-hull distances
       p <- xu
-      p2 <- xu
       pg <- xg
-      coordinates(p) <- c("pc1", "pc2")
-      coordinates(p2) <- c("sPC1", "sPC2")
+      coordinates(p) <- c("PC1", "PC2")
       coordinates(pg) <- c("x", "y")
       dst <- as.vector(gDistance(p, as(sp, "SpatialLines"), byid=T))
-      dst2 <- as.vector(gDistance(p2, as(sp2, "SpatialLines"), byid=T))
       dstg <- as.vector(gDistance(pg, as(spg, "SpatialLines"), byid=T))
       
       # point-sample neighbors
-      nn <- get.knnx(xu[,c("pc1", "pc2")], x[,c("pc1", "pc2")], k=1)
-      nn2 <- get.knnx(xu[,c("sPC1", "sPC2")], x[,c("sPC1", "sPC2")], k=1)
+      nn <- get.knnx(xu[,c("PC1", "PC2")], x[,c("PC1", "PC2")], k=1)
       nng <- get.knnx(xg[,c("x", "y")], x[,c("x", "y")], k=1)
       x$dst <- dst[nn$nn.index[,1]]
-      x$dst2 <- dst2[nn2$nn.index[,1]]
       x$dstg <- dstg[nng$nn.index[,1]]
-      
-      
       
       box$x <- x
       box$sp <- sp
-      box$sp2 <- sp2
       box$spg <- spg
       return(box)
 }
@@ -151,7 +142,7 @@ hulls <- function(box){
 
 
 plots <- function(box){
-      #box <- r[[2]]
+      #box <- r[[4]]
       x <- box$x
       
       
@@ -162,7 +153,7 @@ plots <- function(box){
       fadeg <- scales::rescale(x$dstg, c(1,0)) ^ 3
       
       x$hex <- x %>%
-            dplyr::select(pc1, pc2) %>%
+            dplyr::select(PC1, PC2) %>%
             colorwheel2d(colors = c("gray80", "yellow", "green", "cyan", "blue",
                                     "magenta", "red"),
                          kernel=function(x) x^.01) %>%
@@ -185,99 +176,86 @@ plots <- function(box){
       sd <- fortify(box$sp)
       sdg <- fortify(box$spg)
       
-      lengthen <- 3
+      l <- box$loadings
       
-      p1 <- ggplot(x, aes(pc1, pc2)) + 
+      lengthen <- 5
+      
+      require(shadowtext)
+      
+      p1 <- ggplot(x, aes(PC1, PC2)) + 
             geom_point(color=x$hex) +
             geom_polygon(data=sd, aes(long, lat),
                          fill=NA, color="black") +
             geom_segment(data=l, aes(x=0, y=0, xend=PC1*lengthen, yend=PC2*lengthen),
-                         color="red") +
-            geom_text(data=l, aes(x=PC1*lengthen+.15, y=PC2*lengthen+.15, label=input), 
-                      color="red", hjust=.5, vjust=1) +
+                         color="gray80") +
+            geom_shadowtext(data=l, aes(x=PC1*lengthen+.15, y=PC2*lengthen+.15, label=input), 
+                      color="black", bg.colour="white", hjust=.5, vjust=1) +
             theme_minimal() +
             coord_fixed()
       
-      p1g <- ggplot(x, aes(pc1, pc2)) + 
+      p1g <- ggplot(x, aes(PC1, PC2)) + 
             geom_point(color=x$hexg) +
             geom_segment(data=l, aes(x=0, y=0, xend=PC1*lengthen, yend=PC2*lengthen),
-                         color="red") +
-            geom_text(data=l, aes(x=PC1*lengthen+.15, y=PC2*lengthen+.15, label=input), 
-                      color="red", hjust=.5, vjust=1) +
+                         color="gray80") +
+            geom_shadowtext(data=l, aes(x=PC1*lengthen+.15, y=PC2*lengthen+.15, label=input), 
+                            color="black", bg.colour="white", hjust=.5, vjust=1) +
             theme_minimal() +
             coord_fixed()
       
       p2 <- ggplot(x, aes(x, y)) +
+            geom_polygon(data=borders, aes(long, lat, group=group),
+                         fill="gray80", color="white") +
             geom_raster(fill=x$hex) +
             theme_void() +
-            coord_fixed(ratio=1.2)
+            coord_fixed(ratio=1.2, xlim=range(x$x), ylim=range(x$y))
       
       p2g <- ggplot(x, aes(x, y)) +
+            geom_polygon(data=borders, aes(long, lat, group=group),
+                         fill="gray80", color="white") +
             geom_raster(fill=x$hexg) +
             geom_polygon(data=sdg, aes(long, lat, group=piece, order=order),
                          fill=NA, color="black") +
             theme_void() +
-            coord_fixed(ratio=1.2)
+            coord_fixed(ratio=1.2, xlim=range(x$x), ylim=range(x$y))
       
       p <- arrangeGrob(p1, p1g, ncol=1)
       p <- arrangeGrob(p2, p, p2g, ncol=3, widths=c(2,1,2))
-      p <- arrangeGrob(textGrob(label=x$spp[1]), p, ncol=1, heights=c(1, 10))
+      p <- arrangeGrob(textGrob(label=box$spp, gp=gpar(fontsize=40)), 
+                       p, ncol=1, heights=c(1, 10))
       
-      png(paste0("E:/edges/edginess/species/", x$spp[1], ".png"), 
+      png(paste0("figures/species/", box$spp, ".png"), 
           width=2000, height=800)
-      grid.draw(p)
-      dev.off()
-      
-      return("boom")
-      
-      ### specific PC space ###
-      
-      # continuous colors
-      fade <- scales::rescale(x$dst2, c(1,0)) ^ 3
-      
-      x$hex <- x %>%
-            dplyr::select(sPC1, sPC2) %>%
-            colorwheel2d(colors = c("gray80", "yellow", "green", "cyan", "blue",
-                                    "magenta", "red"),
-                         kernel=function(x) x^.01) %>%
-            col2rgb() %>%
-            t() %>% 
-            "*"(fade) %>%
-            rgb(maxColorValue=255)
-      
-      
-      
-      lengthen <- 3
-      p1 <- ggplot(x, 
-                   aes(sPC1, sPC2)) + 
-            geom_point(color=x$hex) +
-            geom_segment(data=box$loadings, aes(x=0, y=0, xend=PC1*lengthen, yend=PC2*lengthen),
-                         color="red") +
-            geom_text(data=box$loadings, aes(x=PC1*lengthen+.15, y=PC2*lengthen+.15, label=input), 
-                      color="red", hjust=.5, vjust=1) +
-            theme_minimal() +
-            coord_fixed()
-      
-      p2 <- ggplot(x, 
-                   aes(x, y)) +
-            geom_raster(fill=x$hex) +
-            theme_minimal() +
-            coord_fixed(ratio=1.2)
-      
-      p <- arrangeGrob(textGrob(label=x$spp[1]), 
-                       arrangeGrob(p2, p1, nrow=1, widths=c(3, 1)), 
-                       ncol=1, heights=c(1, 10))
-      png(paste0("E:/edges/edginess/species/", x$spp[1], " spc.png"), 
-          width=1000, height=1000)
       grid.draw(p)
       dev.off()
 }
 
-r <- lapply(d, niche)
-r <- lapply(r, hulls)
-for(i in 1:length(r)) r[[i]]$x$spp <- names(r)[i]
-lapply(r, plots)
 
+sp <- c("Quercus douglasii", "Pseudotsuga menziesii", "Pinus albicaulis", "Pinus coulteri")
+
+r <- sp[4] %>% niche() %>% hulls() %>% plots()
+
+
+sp <- basename(dirname(spf))
+
+library(doParallel)
+cl <- makeCluster(detectCores()-1)
+registerDoParallel(cl)
+
+r <- foreach(x = sp) %dopar% {
+      
+      library(tidyverse)
+      library(raster)
+      library(rgdal)
+      library(alphahull)
+      library(rgeos)
+      library(colormap)
+      library(grid)
+      library(gridExtra)
+      
+      x %>% niche() %>% hulls() %>% plots()
+}
+
+stopCluster(cl)
 
 
 
